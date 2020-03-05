0501a89d40b716eeb2fbb8f65abc70cb450e35af
Rodrigo Saravia <rodrigosaraviaalquez@gmail.com>
2020-03-05 18:17:14 -0400

    aaaaa

--- prubea/src/clases/ej1.java
+++ prubea/src/clases/ej1.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
-//Hola
+//HolaAaaaaa
 public class ej1 {
 		protected String idUsuario;
 		protected String genero;
